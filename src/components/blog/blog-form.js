a88c0bced0a4a5f47e6c5805dd820f289960db96
nelidarodriguez <nerodriguez.az9@gmail.com>
2024-09-25 17:41:45 +0200

    make it better

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import DropzoneComponent from "react-dropzone-component";
 
 //import filepickerCss from "../../../node_modules/react-dropzone-component/styles/filepicker.css";
-import dropzoneCss from "../../../node_modules/dropzone/dist/min/dropzone.min.css";
+//import dropzoneCss from "../../../node_modules/dropzone/dist/min/dropzone.min.css";
 
 import RichTextEditor from "../forms/rich-text-editor";
 
